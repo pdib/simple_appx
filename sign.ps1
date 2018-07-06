c51d0c2019d5a02f343a57af775a174a2cb2d4bd
Paul Dib <padib@microsoft.com>
2018-07-06 12:43:50 +0200

    Fix the sign script

--- sign.ps1
+++ sign.ps1
@@ -2,4 +2,4 @@ $password = Read-Host -AsSecureString "Private Key password: "
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
 $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-signtool sign /a /v /debug /fd SHA256 /f $PSScriptRoot\MyKey.pfx  $PSScriptRoot\appbundle.appx /p "${UnsecurePassword}"
+signtool sign /a /v /debug /fd SHA256  /p "${UnsecurePassword}" /f $PSScriptRoot\MyKey.pfx  $PSScriptRoot\appbundle.appx
